7f533b4441b045050784992a6b13d831395006d0
Carmen Zhang <jwzhang06@gmail.com>
2023-04-23 18:51:47 -0400

    contract

--- smartContract.sol
+++ smartContract.sol
@@ -1,12 +1,35 @@
 pragma solidity ^0.8.0;
 
+// TODO: 用 Go 去 调用 智能合约
+
 contract Insurance {
 
     address private owner;
-    int private petId; // the ID of the dog to put insurance on 
-    int private policyNumber; // which plan did the pet owner choose for the pet
+    int private petId; // make it into a map
+    mapping(address => uint[]) public pets; // pets of the owners
+    // key: ownerID
+    // value: petIDs
+
+    struct Record {
+        address owner; // owner of the pet
+        uint petID;  // ID of the pet
+        string recordType; // is it a vaccination, vet's appointment, etc? 
+        uint date; // date of the vaccination, or appointment
+        string additionalInfo; // any additional information about this current record
+    }
+    
 
     event OwnerSet(address indexed oldOwner, address indexed newOwner);
+
+    // Problem 1: 
+    // check pet and owner --> use map 
+
+    // TODO: pet and record (map)
+
+    // Problem 2: Function
+    // TODO: add pet (pet.appendToOwner)
+    //        change owner
+    //          add record 
 
     modifier isOwner() {
         require(msg.sender == owner, "Caller is not owner");
