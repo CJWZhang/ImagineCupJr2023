8640dfe1d28ad6349bb02c9fe66f771705d7c272
Carmen Zhang <jwzhang06@gmail.com>
2023-04-28 09:19:38 -0400

    draft

--- smartContract.sol
+++ smartContract.sol
@@ -4,15 +4,6 @@ pragma solidity ^0.8.0;
 
 contract Insurance {
 
-    struct Pet{
-        uint petID; // RFID tag id
-        string name;
-        string breed;
-        string dob; // date of birth
-        string gender;
-
-    }
-
     struct Record {
         uint num; // the numbering of the record 
         uint petID;  // ID of the pet
@@ -21,13 +12,27 @@ contract Insurance {
         string additionalInfo; // any additional information about this current record
 
     }
-    
-    address private owner; // is this contract owner or pet owner?
 
-    mapping(address => mapping(uint => Record[])) public records; // tracks all the records of the pets of the owner
-    // main key: address of owner 
-    // map 2 key: petIDs
-    // map 2 value: array of records
+    struct Pet{
+        uint petID; // RFID tag id
+        string name;
+        string breed;
+        string dob; // date of birth
+        string gender;
+
+        Record[] petRecords; // records of the pet
+
+    }
+
+    
+    address private owner; 
+    // is this contract owner or pet owner? --> This is the contract owner which I believe to be the pet owner, or the person calling the contract?
+
+    mapping(address => Pet[]) public records; 
+    // tracks all the records of the pets of the owner
+    // key: address of owner 
+    // value: array of their pets
+
 
     event OwnerSet(address indexed oldOwner, address indexed newOwner);
 
@@ -41,6 +46,7 @@ contract Insurance {
         emit OwnerSet(address(0), owner);
     }
 
+
     function changeOwner(address newOwner) public isOwner {
         emit OwnerSet(owner, newOwner);
         owner = newOwner;
@@ -52,55 +58,36 @@ contract Insurance {
 
 
     // add pet to current owner
-    // 没有这个isValue方法吧，利用length判断不为空？
     function addToOwner(uint _petID) public  returns (address){
-        // if(!records[msg.sender][_petID].isValue){
-        //     records[msg.sender][_petID];
-        // }
+        // if(records[msg.sender][_petID].length != 0){
+        if(records[msg.sender][_petID].petRecords.length != 0){
+            records[msg.sender][_petID];
+        }
     }
 
     // change the owner of the pet to newOwner
     function changePetOwner(address newOwner, uint _petID) public  returns (address){
-        Record[] storage currPetRecord = records[msg.sender][_petID];
-        records[newOwner][_petID] = currPetRecord;
+        // Record[] storage currPetRecord = records[msg.sender][_petID];
+        Record[] storage currPetRecord = records[msg.sender][_petID].petRecords;
+        // 这里不对
+        // records[newOwner][_petID] = currPetRecord;
+        
     }
 
     // add record to petID
     function addRecord(uint _petID, string memory _recordType, uint _date, string memory _additionalInfo) public  returns(address) {  
-        uint oldNum = records[msg.sender][_petID].length;
-        // 没有add方法，给你贴了一个例子 在最后
-        records[msg.sender][_petID].add(Record(
-        {
-            num: oldNum + 1, 
-            petID: _petID,
-            recordType: _recordType,
-            date: _date, 
-            additionalInfo: _additionalInfo
-        }
-    ));
+
+        Pet storage p = records[msg.sender][_petID];
+
+        Record memory newRecord;
+        newRecord.petID = _petID;
+        newRecord.recordType = _recordType;
+        newRecord.date = _date;
+        newRecord.additionalInfo = _additionalInfo;
+        // 
+        p.petRecords.push(newRecord);
+
 
     }
 
 } 
-
-//  struct Campaign {
-//         address beneficiary;
-//         uint fundingGoal;
-//         uint numFunders;
-//         uint amount;
-//         mapping (uint => Funder) funders;
-//     }
-// ​
-//     uint numCampaigns;
-//     mapping (uint => Campaign) campaigns;
-// ​
-//     function newCampaign(address payable beneficiary, uint goal) public returns (uint campaignID) {
-//         // campaignID 作为一个变量返回,也就是说不一定要有return，而且函数的返回值定义中也可以初始化变量。
-//         campaignID = numCampaigns++; 
-// ​
-//         // 不能使用 "campaigns[campaignID] = Campaign(beneficiary, goal, 0, 0)"
-//         // 因为RHS会创建一个包含映射的内存结构体 "Campaign"，而映射只能是storage
-//         Campaign storage c = campaigns[campaignID];
-//         c.beneficiary = beneficiary;
-//         c.fundingGoal = goal;
-//     }
